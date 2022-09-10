e2268cd7f1d62d94168217288fdbc7d053868ea2
Anshuman <anshuman8swain@gmail.com>
2022-09-10 09:33:44 +0530

    setup for gh-pages

--- next.config.js
+++ next.config.js
@@ -3,8 +3,12 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
+    loader: 'akamai',
+    path: '',
     domains: ['img.icons8.com'],
   },
+  basePath: '/github-duel',
+  assetPrefix: '/github-duel',
 }
 
 module.exports = nextConfig
